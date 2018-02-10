87f71722a9f7f2caf673868326484c0d65d9febb
quannm <quannm@ezqua.com>
2018-02-10 15:24:57 +0700

    doing add feed

--- src/app/components/add-feed/add-feed.component.ts
+++ src/app/components/add-feed/add-feed.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from './../../api/models/user';
+import { Component, OnInit, Input } from '@angular/core';
 import { App, NavController } from 'ionic-angular';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-add-feed',
@@ -7,8 +9,45 @@ import { App, NavController } from 'ionic-angular';
 })
 export class AddFeedComponent implements OnInit {
 
-  constructor(public nav: NavController, public appCtrl: App) {}
+  @Input('content') content: string;
+  public is_show_tag: boolean;
+  public is_show_autocomplete: boolean = false;
+  public users: Array<User>;
+  public usersTmp: Array<User>;
+  public usersTag: Array<User> = [];
+  constructor(public nav: NavController, public appCtrl: App, private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getFriends(null).subscribe(data => {
+      this.users = data;
+      this.usersTmp = data;
+    })
+  }
+
+  showTagFriend() {
+    this.is_show_tag = !this.is_show_tag;
+  }
+
+  chooseUserTag(user: User) {
+    this.usersTag.push(user);
+    this.content = '';
+    this.is_show_autocomplete = false;
+  }
+
+  removeUserTag(user: User) {
+    this.usersTag = this.usersTag.filter(e => e.guid != user.guid);
+  }
+
+  getItems(ev: any) {
+    let val = ev.target.value;
+    this.usersTmp = this.users;
+    if (val && val.trim() != '') {
+      this.is_show_autocomplete = true;
+      this.usersTmp = this.usersTmp.filter((item) => {
+        return (item.fullname.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    } else {
+      this.is_show_autocomplete = false;
+    }
   }
 }
