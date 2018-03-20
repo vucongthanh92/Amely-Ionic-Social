bc0474ba3056b2c1da806b0a07c1d605a7cf8b28
quannm <quannm@ezqua.com>
2018-03-20 10:05:16 +0700

    comment keyboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { Keyboard } from '@ionic-native/keyboard';
+// import { Keyboard } from '@ionic-native/keyboard';
 import { SigninComponent } from './authentication/signin/signin.component';
 
 
@@ -15,25 +15,25 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
   rootPage: any = SigninComponent;
 
-  pages: Array<{title: string, component: any,image: string}>;
-  
+  pages: Array<{ title: string, component: any, image: string }>;
+
   constructor(
     public platform: Platform,
     public statusBar: StatusBar,
     public splashScreen: SplashScreen,
     public menuCtrl: MenuController,
-    public keyboard: Keyboard
+    // private keyboard: Keyboard
     // private cordova: cordova
   ) {
     this.initializeApp();
   }
 
-  
+
 
   initializeApp() {
     // cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     this.platform.ready().then(() => {
-      this.keyboard.hideKeyboardAccessoryBar(false)
+      // this.keyboard.hideKeyboardAccessoryBar(false)
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
