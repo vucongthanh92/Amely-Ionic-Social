a549dacfd81d83e1317b0efc2303e6b3ac304a84
quannm <quannm@ezqua.com>
2018-03-28 15:35:32 +0700

    test contact cordova

--- src/app/views/personal/personal-menu/personal-menu.component.ts
+++ src/app/views/personal/personal-menu/personal-menu.component.ts
@@ -2,16 +2,20 @@ import { User } from './../../../api/models/user';
 import { Component, OnInit } from '@angular/core';
 import { NavController, App } from 'ionic-angular';
 import { UserComponent } from '../../../components/user/user.component';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
 
 @Component({
   selector: 'app-personal-menu',
-  templateUrl: './personal-menu.component.html'
+  templateUrl: './personal-menu.component.html',
+  providers:[
+    Contacts
+  ]
 })
 
 export class PersonalMenuComponent implements OnInit {
 
   public userCurrent: User;
-  constructor(public nav: NavController, private appCtrl: App) {
+  constructor(public nav: NavController, private appCtrl: App, private contacts: Contacts) {
     this.userCurrent = JSON.parse(localStorage.getItem("loggin_user"));
   }
 
@@ -23,4 +27,16 @@ export class PersonalMenuComponent implements OnInit {
     this.appCtrl.getRootNav().push(UserComponent, { userGuid: this.userCurrent.guid });
   }
 
+  updateContacts(){
+    this.contacts.find(["name", "phoneNumbers"],
+      { multiple: true}).then((contacts) => {
+        console.log(contacts);
+        
+        if (contacts.length != 0) {
+        
+        } else {
+        
+        }
+      });
+  }
 }
