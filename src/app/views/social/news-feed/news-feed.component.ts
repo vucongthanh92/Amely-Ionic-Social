7ca054d2f9b471cb20e055cb70eb2d67231df854
quannm <quannm@ezqua.com>
2018-05-08 13:26:30 +0700

    fix fab new feed

--- src/app/views/social/news-feed/news-feed.component.ts
+++ src/app/views/social/news-feed/news-feed.component.ts
@@ -1,15 +1,18 @@
+import { FeedsComponent } from './../../../components/feeds/feeds.component';
 import { CustomService } from './../../../services/custom.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { App, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'app-news-feed',
   templateUrl: './news-feed.component.html',
+ 
 })
 
 export class NewsFeedComponent implements OnInit {
- feed_type = "home";
+  feed_type = "home";
   url1;
+  @ViewChild('feeds') feeds: FeedsComponent; 
   constructor(
     public nav: NavController,
     public appCtrl: App,
@@ -17,14 +20,16 @@ export class NewsFeedComponent implements OnInit {
   ) {
     // this.url ='https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
     // this.url1 = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
-      // document.getElementById('innerframetmp');
-   
-   }
+    // document.getElementById('innerframetmp');
+
+  }
 
   ngOnInit() { }
   // byPassUrl() {
   //   return this.sanitizer.bypassSecurityTrustResourceUrl(this.url);	
   // }
 
-
+  addFeed() {
+    this.feeds.addNewFeed()
+  }
 }
