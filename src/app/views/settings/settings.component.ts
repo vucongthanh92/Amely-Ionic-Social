d2d9ee24113590680e62fd5e40e372e4162aac20
thanhvc <thanhvc@ezqua.com>
2018-05-25 17:41:00 +0700

    fix css iphonex ver2

--- src/app/views/settings/settings.component.ts
+++ src/app/views/settings/settings.component.ts
@@ -23,20 +23,7 @@ export class SettingsComponent implements OnInit {
     public appCtrl: App, 
     public navParams:NavParams,
     private customService:CustomService) {
-      var ratio = window.devicePixelRatio || 1;
-      var screen = {
-        width: window.screen.width * ratio,
-        height: window.screen.height * ratio
-      };
-      if (screen.width == 1125 && screen.height == 2436) {
-        this.device_screen = "top_navigation_iphonex";
-      }
-      else if (screen.width == 1242 && screen.height == 2208) {
-        this.device_screen = "top_navigation_iphone7plus";
-      }
-      else {
-        this.device_screen = "top_navigation_iphone6s";
-      }
+      this.device_screen = customService.checkDevices();
     }
 
   ngOnInit() {
