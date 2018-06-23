b8f80d1f93c273671ba17975d6093e2a3e25a45c
thanhvc <thanhvc@ezqua.com>
2018-06-23 17:41:20 +0700

    fix css amely

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NavParams, Platform } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { NavController } from 'ionic-angular';
+
 import {
   GoogleMaps,
   GoogleMap,
   GoogleMapsEvent,
   GoogleMapOptions,
 } from '@ionic-native/google-maps';
+
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html'
 })
+
 export class LocationComponent implements OnInit {
   @ViewChild('map') element;
   map: GoogleMap;
@@ -18,8 +22,12 @@ export class LocationComponent implements OnInit {
   lng: number;
   title: string;
 
-  constructor(public geolocation: Geolocation,
-    public navParams: NavParams, platform: Platform) {
+  constructor(
+    public geolocation: Geolocation,
+    public navParams: NavParams, 
+    platform: Platform,
+    public nav: NavController
+  ) {
     this.lat = this.navParams.get('lat');
     this.lng = this.navParams.get('lng');
     this.title = this.navParams.get('title');
@@ -59,9 +67,9 @@ export class LocationComponent implements OnInit {
         }).then(marker => { });
       })
       .catch(e => console.log(e));
-
-
   }
 
-
+  dismiss() {
+    this.nav.pop();
+  }
 }
